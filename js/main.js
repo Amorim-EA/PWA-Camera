94bee49999ba44e9ad16b1172db966ea3ddab453
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-10-30 17:47:37 -0400

    add indexedDB...Alert("have erros, an it is incomplete the code");

--- js/main.js
+++ js/main.js
@@ -11,8 +11,54 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+//importando indexedDB
+import { openDB } from "idb";
+
+let db;
+async function criarDB(){
+    try {
+        db = await openDB('banco', 1, {
+            upgrade(db, oldVersion, newVersion, transaction){
+                switch  (oldVersion) {
+                    case 0:
+                    case 1:
+                        const store = db.createObjectStore('fotos', {
+                            keyPath: 'titulo'
+                        });
+                        store.createIndex('id', 'id');
+                        console.log("Banco criado!");
+                }
+            }
+        });
+        console.log("Banco aberto!");
+    }catch (e) {
+        console.log('Erro ao criar/abrir banco: ' + e.message);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async event =>{
+  criarDB();
+});
+
+async function guardarFoto(urlToPhoto) {
+  const tx = await db.transaction('fotos', 'readwrite');
+  const store = tx.objectStore('fotos');
+  try {
+      await store.add({ 
+          url: urlToPhoto
+      });
+      await tx.done;
+      console.log('Foto adicionado com sucesso!');
+  } catch (error) {
+      console.error('Erro ao adicionar:', error);
+      tx.abort();
+  }
+}
+
+
 // configurando as constraintes do video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
+
 // capturando os elementos em tela
 const cameraView = document.querySelector("#camera--view");
 const cameraOutput = document.querySelector("#camera--output");
@@ -24,7 +70,8 @@ function cameraStart() {
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (stream) {
-      track = stream.getTracks()[0]; 
+      let track = stream.getTracks()[0];
+      console.log(track); 
       cameraView.srcObject = stream;
   })
   .catch(function (error) {
@@ -35,8 +82,9 @@ function cameraStart() {
 // Função para tirar foto
 cameraTrigger.onclick= function () {
   cameraSensor.width = cameraView.videoWidth;
-  cameraSensor.height = cameraView.videoHeight; cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraSensor.height = cameraView.videoHeight; 
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/png");
   cameraOutput.classList.add("taken");
 };
 
