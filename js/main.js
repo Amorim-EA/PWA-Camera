9bfc256d3dddfdf1c48d3440b7feda7b851c463a
Amorim-EA <erick.amorim@estudanmte.ifms.edu.br>
2023-11-13 17:47:07 -0400

    Trocar camera adicionado.

--- js/main.js
+++ js/main.js
@@ -11,59 +11,17 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-//importando indexedDB
-import { openDB } from "idb";
-
-let db;
-async function criarDB(){
-    try {
-        db = await openDB('banco', 1, {
-            upgrade(db, oldVersion, newVersion, transaction){
-                switch  (oldVersion) {
-                    case 0:
-                    case 1:
-                        const store = db.createObjectStore('fotos', {
-                            keyPath: 'titulo'
-                        });
-                        store.createIndex('id', 'id');
-                        console.log("Banco criado!");
-                }
-            }
-        });
-        console.log("Banco aberto!");
-    }catch (e) {
-        console.log('Erro ao criar/abrir banco: ' + e.message);
-    }
-}
-
-window.addEventListener('DOMContentLoaded', async event =>{
-  criarDB();
-});
-
-async function guardarFoto(urlToPhoto) {
-  const tx = await db.transaction('fotos', 'readwrite');
-  const store = tx.objectStore('fotos');
-  try {
-      await store.add({ 
-          url: urlToPhoto
-      });
-      await tx.done;
-      console.log('Foto adicionado com sucesso!');
-  } catch (error) {
-      console.error('Erro ao adicionar:', error);
-      tx.abort();
-  }
-}
-
-
 // configurando as constraintes do video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+let camMode = 'user';
+var constraints = { video: { facingMode: camMode }, audio: false };
 
 // capturando os elementos em tela
-const cameraView = document.querySelector("#camera--view");
-const cameraOutput = document.querySelector("#camera--output");
-const cameraSensor = document.querySelector("#camera--sensor");
-const cameraTrigger = document.querySelector("#camera--trigger");
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger"),
+  cameraSwitcher = document.querySelector("#camera--switcher");
+
 
 //Estabelecendo o acesso a camera e inicializando a visualização 
 function cameraStart() {
@@ -87,6 +45,22 @@ cameraTrigger.onclick= function () {
   cameraOutput.src = cameraSensor.toDataURL("image/png");
   cameraOutput.classList.add("taken");
 };
+
+
+cameraSwitcher.onclick = function (){
+  stopMediaTracks(cameraView.srcObject);
+  camMode = camMode === 'user' ? 'environment' : 'user';
+  constraints = { video: { facingMode: camMode }, audio: false };
+  cameraStart();
+}
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach((track) => {
+    track.stop();
+  });
+
+}
+
 
 // carrega imagem de camera quando a janela carregar 
 window.addEventListener("load", cameraStart, false);
